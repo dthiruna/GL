315a70dafabbef627808276fcae1e629d7625ec4
d thirunavukkarasu <d.thirunavukkarasu@ge1.iut-cachan.u-psud.fr>
2018-11-14 19:10:37 +0100

    j'ai emarré exo 43

--- src/main.c
+++ src/main.c
@@ -1,9 +1,23 @@
-//  Copyright (c) 2018 Antoine Tran Tan
-//
+#include <math.h>
 
-#include "my_header.h"
-
-int main(void)
+void main (void)
 {
-    return 0;
+	unsigned short S1=0;
+	unsigned short S2=0;
+	unsigned short n=0;
+	unsigned short n2=0;
+
+	while(n<10)
+	{
+		S1=3*n+S1;
+		n++;
+	}
+	
+	while((3*n2)<9876)
+	{
+     	S2=3*n2+S2;
+		n2++;
+	}
+		
+	
 }
