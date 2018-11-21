d32b7ef339989d83335e3b8eec5f22b8b969c9cf
dthiruna <44802273+dthiruna@users.noreply.github.com>
2018-11-21 23:36:18 +0100

    Update my_header.h
    J'ai eu un peu du mal à terminer l'exercice, puis je n'ai pas très bien compris la notion de RAND.

--- include/my_header.h
+++ include/my_header.h
@@ -1,9 +1,48 @@
-//  Copyright (c) 2018 Antoine Tran Tan
+ //  Copyright (c) 2018 Antoine Tran Tan
 //
 
-#ifndef MY_HEADER_H
-#define MY_HEADER_H
+#include <ansi_c.h>
+#include "my_header.h"
 
 
+int main(void)
+{
+    unsigned char bin[10]={12,3,2,5,17,15,27,6,19,10},i,seuil=18;
+    int sat[10]={rand()%100-50,rand()%100-50,rand()%100-50,rand()%100-50,rand()%100-50,rand()%100-50,rand()%100-50,rand()%100-50,rand()%100-50,rand()%100-50},n,SEUILB=-100,SEUILH=200;
+    int dist[10]={45,35,26,12,8,65,36,22,13,10},m,distance;
+    
+	for(i=0;i<10;i++)
+		
+    {
+        if(bin[i]>seuil)
+        {
+            bin[i]=1;
+        }
+        else
+        {
+            bin[i]=0;
+        }
+    }
 
-#endif
+    for(n=0;n<10;n++)
+    {
+        if(sat[n]>SEUILH)
+        {
+            sat[n]=SEUILH;
+        }else if(sat[n]<SEUILB)
+        {
+            sat[n]=SEUILB;
+        }
+    }
+
+
+    for(m=0;m>10;m--)
+    {
+        distance=abs(dist [m]-dist[m-1]);
+        dist[m]=distance;
+    }
+
+
+  
+    return 0;
+}
