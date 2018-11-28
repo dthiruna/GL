6c2e032ca47fb179db2f5ad1dd8e8c6567c750f9
dthiruna <44802273+dthiruna@users.noreply.github.com>
2018-11-28 23:39:42 +0100

    Update main.c
    Je n'ai pas réussi à le mettre sur Github mais j'ai tout de même essayé mais pour être sur que je vous l'avez reçu je vous l'envoie ici aussi.

--- src/main.c
+++ src/main.c
@@ -1,9 +1,34 @@
-//  Copyright (c) 2018 Antoine Tran Tan
-//
+ //  Copyright (c) 2018 Antoine Tran Tan
+
 
 #include "my_header.h"
+#include "TP_lib_suite.h"
+void  initialiserTirage(void);
+short tirerNumero(void);
 
 int main(void)
 {
+    unsigned char tableau1 [6]={12,33,15,17,46,24},tableaugagnant [6],valeur=0,s,n,i,q;
+    for(q=0;q<100;q++)
+    {
+        for(s=0;s<6;s++)
+     {
+        initialiserTirage();
+        tableaugagnant[s]=tirerNumero();
+     }
+
+        for(i=0;i<6;i++)
+     {
+        for(n=0;n<6;n++)
+        {
+            if(tableau1 [i]==tableaugagnant[n])
+            {
+                valeur=valeur+1;
+            }
+
+        }
+     }
+
+    }
     return 0;
 }
