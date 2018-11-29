b0b89013f27f73cc121086faf6eca39b10ac4089
d thirunavukkarasu <d.thirunavukkarasu@ge1.iut-cachan.u-psud.fr>
2018-11-29 15:21:44 +0100

    jai change le fichier

--- src/main.c
+++ src/main.c
@@ -1,34 +1,28 @@
- //  Copyright (c) 2018 Antoine Tran Tan
+//  Copyright (c) 2018 Antoine Tran Tan
 
 
 #include "my_header.h"
 #include "TP_lib_suite.h"
-void  initialiserTirage(void);
-short tirerNumero(void);
 
 int main(void)
 {
-    unsigned char tableau1 [6]={12,33,15,17,46,24},tableaugagnant [6],valeur=0,s,n,i,q;
+    unsigned char joueur [6]={12,33,15,17,46,24},gagnant[6]={0,0,0,0};
+	int q=0, s=0, n=0, i=0;
+	
+
     for(q=0;q<100;q++)
     {
+		
         for(s=0;s<6;s++)
+			
      {
         initialiserTirage();
-        tableaugagnant[s]=tirerNumero();
+		
+       gagnant[s] = tirerNumero();
      }
 
-        for(i=0;i<6;i++)
-     {
-        for(n=0;n<6;n++)
-        {
-            if(tableau1 [i]==tableaugagnant[n])
-            {
-                valeur=valeur+1;
-            }
-
-        }
-     }
-
+	
+	 
     }
     return 0;
 }
